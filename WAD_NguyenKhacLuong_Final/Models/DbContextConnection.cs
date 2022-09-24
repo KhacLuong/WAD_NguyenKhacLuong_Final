9d79a57bdc520ec9b8730713b53f7ce4ba1505b3
PC <PC@ADMIN>
2022-09-24 19:34:19 +0700

    Add project files.

---
+++ WAD_NguyenKhacLuong_Final/Models/DbContextConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WAD_NguyenKhacLuong_Final.Models
+{
+    public class DbContextConnection:DbContext
+    {
+        public DbContextConnection() : base("name=Dbconnection")
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+        
+    }
+}
---
+++ WAD_NguyenKhacLuong_Final/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WAD_NguyenKhacLuong_Final.Models
+{
+    public class Employee
+    {
+        [Key]
+        [Display(Name = "EmployeeID")]
+        [Required(ErrorMessage = "Không được bỏ qua trường này")]
+        public String Id { get; set; }
+
+        [Display(Name = "EmployeeName")]
+        [Required(ErrorMessage = "Không được bỏ qua trường này")]
+        public String Name { get; set; }
+
+        [Required(ErrorMessage = "Không được bỏ qua trường này")]
+        public String Departmet { get; set; }
+
+        [Required(ErrorMessage = "Không được bỏ qua trường này")]
+        [DataType(DataType.Currency)]
+        [RegularExpression(@"^\d+.?\d{0,2}$", ErrorMessage = "Nhập với định dạng tiền tệ vd: 10000,00")]
+        public Decimal Salary { get; set; }
+    }
+}
